ba0b3148d2d3c3381bdeccac0fddff9dc225c3d3
jj273825 <61196143+jj273825@users.noreply.github.com>
2021-02-01 10:43:14 +0800

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 10:02:29 2021
+
+@author: MTAEXAM
+"""
+top = float(input("上底:"))
+bottom = float(input("下底:"))
+height = float(input("高:"))
+area = (top+bottom)*height/2
+print("梯形面積:" + str(area))
